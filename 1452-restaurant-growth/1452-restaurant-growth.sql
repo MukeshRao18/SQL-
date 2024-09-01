f8d2ba9c44dc367742fa0a1d79cf8661f0fdae5e
Beerkoor Mukesh Kumar <138296745+MukeshRao18@users.noreply.github.com>
2024-09-01 22:40:11 +0530

    Time: 1115 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1452-restaurant-growth/1452-restaurant-growth.sql
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -1,21 +1,13 @@
-SELECT
-    visited_on,
-    (
-        SELECT SUM(amount)
-        FROM customer
-        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
-    ) AS amount,
-    ROUND(
-        (
-            SELECT SUM(amount) / 7
-            FROM customer
-            WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
-        ),
-        2
-    ) AS average_amount
-FROM customer c
-WHERE visited_on >= (
-        SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
-        FROM customer
-    )
-GROUP BY visited_on;
+with cte as(
+select 
+visited_on,
+sum(amount) as amount
+from Customer
+group by visited_on
+order by visited_on
+)
+
+select visited_on, sum(amount) over(rows between 6 preceding and current row)as amount
+,round(avg(amount) over(rows between 6 preceding and current row),2) as average_amount
+ from cte
+ limit 6,9999;
