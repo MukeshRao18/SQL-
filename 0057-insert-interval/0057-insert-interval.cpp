8bb4718da8f83af5bf316bd0048aeeda8848a4c8
Beerkoor Mukesh Kumar <138296745+MukeshRao18@users.noreply.github.com>
2024-09-07 23:02:44 +0530

    Time: 11 ms (60.82%), Space: 20.8 MB (33.23%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals,vector<int>& newInterval) {
-        vector<vector<int>> answer;
-        int a1 = newInterval[0], b1 = newInterval[1];
-
-        for (int i = 0; i < intervals.size(); ++i) {
-            int a2 = intervals[i][0], b2 = intervals[i][1];
-
-            if (a2 > b1) {
-                answer.push_back({a1, b1});
-                a1 = a2, b1 = b2;
-            } else if (a1 > b2) { 
-                answer.push_back({a2, b2});
-            } else {
-                int a = min(a1, a2), b = max(b1, b2);
-                a1 = a, b1 = b;
-            }
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newinterval) {
+        int i=0,n=intervals.size();
+        vector<vector<int>> res;
+        while(i<n){
+           while(i<n&&intervals[i][1]<newinterval[0]){
+            res.push_back(intervals[i]);
+            i++;
+           }
+           while(i<n&& intervals[i][0]<=newinterval[1]){
+            newinterval[0]=min(intervals[i][0],newinterval[0]);
+            newinterval[1]=max(intervals[i][1],newinterval[1]);
+            i++;
+           }
+           res.push_back({newinterval[0],newinterval[1]});
+           while(i<n){
+            res.push_back(intervals[i]);
+            i++;
+           }
         }
-
-        answer.push_back({a1, b1});
-
-        return answer;
+        if(n==0){
+            res.push_back({newinterval[0],newinterval[1]});
+        }
+        return res;
     }
 };
