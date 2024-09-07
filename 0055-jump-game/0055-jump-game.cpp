289e854849596311832be10bc685ea62931b73db
Beerkoor Mukesh Kumar <138296745+MukeshRao18@users.noreply.github.com>
2024-09-07 11:16:50 +0530

    Time: 601 ms (9.26%), Space: 54.2 MB (5.91%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        vector<int> memo(nums.size(), -1); 
+        
+        return solve(nums, 0, nums.size() - 1, memo);
+    }
+    
+    bool solve(vector<int>& nums, int idx, int n, vector<int>& memo) {
+        if (idx == n) {
+            return true;
+        }
+        
+        
+        if (memo[idx] != -1) {
+            return memo[idx];
+        }
+        
+        if (nums[idx]) {
+            for (int i = 1; i <= nums[idx]; i++) {
+                if (solve(nums, idx + i, n, memo)) {
+                    return memo[idx] = true; 
+                }
+            }
+        } 
+        
+        return memo[idx] = false; 
+    }
+};
